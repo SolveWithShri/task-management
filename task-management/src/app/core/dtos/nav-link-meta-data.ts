60cd3aa74ec36f3e7d0595d383dc3afbc3fcfd1e
shrikant <s.adhav@easternenterprise.com>
2019-09-16 16:21:19 +0530

    configurable link

---
+++ task-management/src/app/core/dtos/nav-link-meta-data.ts
@@ -0,0 +1,5 @@
+export interface NavLinkMetaData {
+    header: string;
+    iconClass: string;
+    link: any[] | string;
+}
--- task-management/src/app/modules/inner-shell/components/side-nav-bar/side-nav-bar.component.ts
+++ task-management/src/app/modules/inner-shell/components/side-nav-bar/side-nav-bar.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
 
+import { NavLinkMetaData } from './../../../../core/dtos/nav-link-meta-data';
+
 @Component({
   selector: 'app-side-nav-bar',
   templateUrl: './side-nav-bar.component.html',
   styleUrls: ['./side-nav-bar.component.scss']
 })
-export class SideNavBarComponent { }
+export class SideNavBarComponent {
+
+  navLinkConfig: NavLinkMetaData[] = [
+    {
+      header: 'Home',
+      iconClass: ' fa fa-home fa-2x',
+      link: ['/home']
+    },
+    {
+      header: 'Tasks',
+      iconClass: ' fa fa-tasks fa-2x',
+      link: ['/tasks']
+    }
+  ];
+}
